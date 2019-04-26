8f2805c0654906b425e1b326db863932e138d033
Alyssa Liu <xl2863@columbia.edu>
2019-04-26 03:15:26 +0000

    Add python file for function to get title

---
+++ title.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+import re
+
+def title(lyric):
+	pattern = 'r([^~]*).txt$'
+	match = re.search(pattern, lyric)
+	title = ' '.join(match.group(0)[:-4].split('-'))
+	return title
+
