5216b536d4ff24de32ddc7037fef7b11ff563293
Ada Zhang <zhaoming19960521@gmail.com>
2019-05-05 02:02:29 +0000

    Analyzing lyrics using love.py

--- love.py
+++ love.py
@@ -1,7 +1,131 @@
-#!/usr/bin/env python
+!/usr/bin/env python
 
-import random
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from textblob import TextBlob
 
+arrLove = [
+    'love',
+    'honey',
+    'sweetheart',
+    'baby',
+    'need'
+    'valentine',
+    'lover',
+    'romance',
+    'romantic',
+    'married',
+    'engaged',
+    'marry',
+    'engage',
+    'blessing',
+    'blessings',
+    'loving',
+    'mine',
+    'rose',
+    'roses',
+    'heart',
+    'adorable',
+    'amazing',
+    'angel',
+    'babe',
+    'beautiful',
+    'beloved',
+    'darling',
+    'dearest',
+    'enchanting',
+    'gorgeous',
+    'handsome',
+    'heavenly',
+    'paramour',
+    'sweetie',
+    'swoon',
+    'wonderful',
+    'adore',
+    'admire',
+    'care',
+    'cherish',
+    'choose',
+    'chosen',
+    'daydream',
+    'delight',
+    'dream',
+    'girl',
+    'need',
+    'prize',
+    'lifetime',
+    'affection',
+    'passion',
+    'amour',
+    'sugar',
+    'yearn',
+    'family',
+    'caring',
+    'happiness',
+    'forever',
+    'happy',
+    'trust',
+    'kiss',
+    'hugs',
+    'warm',
+    'fun',
+    'kisses',
+    'joy',
+    'sex',
+    'friendship',
+    'marriage',
+    'hug',
+    'amore',
+    'desire',
+    'hope',
+    'like',
+    'liking',
+    'loving',
+    'respect',
+    'wife',
+    'husband',
+    'faithful',
+    'appreciate',
+    'friends',
+    'fond',
+    'fondness',
+    'favourite',
+    'bliss',
+    'chocolate',
+    'darling',
+    'dear',
+    'intimate',
+    'intimacy',
+    'beloved',
+    'breathtaking',
+    'bright',
+    'brilliant',
+    'crazy',
+    'dedicated',
+    'devotion',
+    'friendly',
+    'gorgeous',
+    'graceful',
+    'incredible',
+    'lovable',
+    'lovely',
+    'lucky',
+    'soulmate',
+    'precious',
+    'attractive',
+    'sweetness']
 def love(data):
-	return round(random.random(), 1)
+    pull_chars = '!;,.?'
+    for pull_char in pull_chars:
+        data = data.replace(pull_char, '')
+    stop = stopwords.words('english')
+    data = " ".join(word for word in data.split() if word not in stop)
+    data = TextBlob(data).words
+    bw = [1 if i.lower() in arrLove else 0 for i in data]
+    k = sum(bw)/len(data)
+    k = k / 0.2
+    k = k // 0.1 / 10
+    return k
+
 
