10a15f3499daa5fd29c77f6e40201bec93ab9ec1
Alyssa Liu <xl2863@columbia.edu>
2019-05-07 00:36:39 +0000

    Modify the comment for complexity

--- complexity.py
+++ complexity.py
@@ -7,7 +7,7 @@ def complexity(data):
 	comp = textstat.flesch_reading_ease(data)	# get the Flesch reading-ease test. In the Flesch reading-ease test, 
 							# higher scores indicate material that is easier to read; 
 							# lower numbers mark passages that are more difficult to read. 
-	comp = round(1-(comp-(-806.84))/(125.32-(-806.84)), 1)	# normalize the score, 
+	comp = round(1-(comp-(-806.84))/(125.32-(-806.84)), 1)	# normalize the score to range [0, 1] 
 								# the original score range for all lyrics is [-806.84, 125.32]
 	return comp
 
